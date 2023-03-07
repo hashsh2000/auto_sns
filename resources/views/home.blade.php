8d5daa6766236dc4a58845188b0e25a270bd5b0c
Kazuya1Hashimoto10 <61010785+Kazuya1Hashimoto10@users.noreply.github.com>
2023-03-08 05:06:28 +0900

    ホーム画面調整

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,24 +14,27 @@
         <li class="list-group-item">
             <h3>メニュー</h3>
         </li>
-        <a href="#" class="text-decoration-none text-dark">
-            <li class="list-group-item">項目1</li>
+        <a href="{{ route('dm.index', ['user_id' => Auth::id()]) }}" class="text-decoration-none text-dark">
+            <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/></svg>
+                アカウント設定
+            </li>
         </a>
-        <a href="#" class="text-decoration-none text-dark">
-            <li class="list-group-item">項目2</li>
+        <a href="{{ route('dm.index', ['user_id' => Auth::id()]) }}" class="text-decoration-none text-dark">
+            <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16"><path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/></svg>
+                DM設定
+            </li>
         </a>
-        <a href="#" class="text-decoration-none text-dark">
-            <li class="list-group-item">項目3</li>
+        <a href="{{ route('keyword.index', ['user_id' => Auth::id()]) }}" class="text-decoration-none text-dark">
+            <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg>
+                検索設定
+            </li>
         </a>
-        <a href="#" class="text-decoration-none text-dark">
-            <li class="list-group-item">項目4</li>
+        <a href="{{ route('dm.index', ['user_id' => Auth::id()]) }}" class="text-decoration-none text-dark" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/><path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/></svg>
+                ログアウト
+            </li>
         </a>
-        <a href="#" class="text-decoration-none text-dark">
-            <li class="list-group-item">項目5</li>
-        </a>
-        <a href="#" class="text-decoration-none text-dark">
-            <li class="list-group-item">項目6</li>
-        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">@csrf</form>
     </ul>
 </div>
 @endsection
